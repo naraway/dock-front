42fbac89061c427d2ed6ed51954b8ef29113644e
Seungbong Lee <mervonee@gmail.com>
2022-08-05 15:25:36 +0900

    :wrench: Changing configuration files. - update configuration for storybook

--- .storybook/config/dev.js
+++ .storybook/config/dev.js
@@ -40,7 +40,7 @@ export const devdock = {
 export const devinterceptors = [{
   request: {
     onFulfilled: (config) => {
-      const roles = ['personalUser', 'teamAdmin', 'teamUser'].join(',');
+      const roles = ['director', 'user', 'pavilion-admin', 'cineroom-admin'].join(',');
       config.headers = {...(config.headers || {}), roles};
       return config;
     },
